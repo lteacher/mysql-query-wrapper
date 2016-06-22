e23946a9d397fb016b06d79cde0a6c936bf231f4
Hugo Armstrong <hugo.armstrong@gmail.com>
2016-06-22 20:22:55 +1000

    Updated all the tests and prepare to publish

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 Another query wrapper for mysql... Probably like most of the other examples, in refactoring code to make it nicer to use the `mysql` package these functions emerged so why not randomly publish them. If you want to use then obviously go for it. Any issues or features create them at github.
 
 ## Features
-Converts the mysql query to one that returns a promise unless a callback is provided
+Converts the mysql query to one that returns a promise unless a callback is provided.
 
-Also adds the functions:
+Also adds the following functions which can take a single column condition:
 - `update` - _Performs an SQL update returning a promise_
 - `insert` - _Performs an SQL insert returning a promise_
 - `modify` - _If the entry exists, updates else inserts returning promise_
 - `exists` - _Check the existence of some entry returning a promise_
 - `query` - _Mysql all purpose query which uses the default callback if provided else returns a promise_
+
+For usage examples take a look in `test/test.js`.
--- test/test.js
+++ test/test.js
@@ -2,12 +2,13 @@
 
 const test = require('ava');
 const db = require('../lib/wrapper')();
+const mysql = require('mysql');
 
 // Setup tables / any data etc
 test.before(async t => {
   // omg async / await booyakasha!
   await db.query('drop table if exists user;');
-  let result = await db.query(
+  await db.query(
     `create table user(
       id int not null auto_increment,
       firstName varchar(255),
@@ -17,8 +18,38 @@ test.before(async t => {
       primary key (id)
     );`
   );
-
-  return result;
+  await db.insert('user', {
+    firstName: 'Bill',
+    lastName: 'singleUpdate'
+  });
+  await db.insert('user', {
+    firstName: 'Jimmy',
+    lastName: 'multiUpdate'
+  });
+  await db.insert('user', {
+    firstName: 'Benny',
+    lastName: 'multiUpdate'
+  });
+  await db.insert('user', {
+    firstName: 'Jamie',
+    lastName: 'singleDeletion'
+  });
+  await db.insert('user', {
+    firstName: 'Johnny',
+    lastName: 'multiDeletion'
+  });
+  await db.insert('user', {
+    firstName: 'Sammy',
+    lastName: 'multiDeletion'
+  });
+  await db.insert('user', {
+    firstName: 'Modify',
+    lastName: 'Me'
+  });
+  await db.insert('user', {
+    firstName: 'Exists',
+    lastName: 'Tester'
+  });
 });
 
 test('Can insert a single entry', async t => {
@@ -31,9 +62,9 @@ test('Can insert a single entry', async t => {
   t.is(result['affectedRows'], 1);
 });
 
-// TODO: This fails and it would be nice if it worked, looks like a change
-// is required for this...
-test.skip('Can insert multiple entries', async t => {
+// Insert uses SET but apparently for muti it needs VALUES? which is
+// a little bit unfortunate
+test.failing('Can insert multiple entries', async t => {
   let users = [{
     firstName: 'Jack',
     lastName: 'Frank'
@@ -43,18 +74,69 @@ test.skip('Can insert multiple entries', async t => {
   }];
 
   let result = await db.insert('user', users);
-  t.is(result['affectedRows'], 2);
+  t.is(result.affectedRows, 2);
 });
 
 test('Can update an existing entry', async t => {
-  let user = {
-    firstName: 'Tobe',
-    lastName: 'Updated'
-  };
+  let result = await db.update('user', {
+    lastName: 'Jones'
+  }, ['lastName', 'singleUpdate']);
+  t.is(result.affectedRows, 1);
+});
 
-  let result = await db.insert('user', user);
-  t.is(result['affectedRows'], 1);
+test('Can update multiple existing entries', async t => {
+  let result = await db.update('user', {
+    lastName: 'Samson'
+  }, ['lastName', 'multiUpdate']);
+  t.is(result.affectedRows, 2);
+});
 
-  result = await db.update('user',{firstName: 'Hasbeen'},['firstName','Tobe']);
-  t.is(result['affectedRows'], 1);
+test('Can delete an existing entry', async t => {
+  let result = await db.delete('user', ['lastName', 'singleDeletion']);
+  t.is(result.affectedRows, 1);
+});
+
+test('Can delete multiple existing entries', async t => {
+  let result = await db.delete('user', ['lastName', 'multiDeletion']);
+  t.is(result.affectedRows, 2);
+});
+
+test('Can modify an existing entry', async t => {
+  let result = await db.modify('user', {
+    firstName: 'Dan',
+    lastName: 'James'
+  }, ['firstName', 'Modify']);
+  t.is(result.affectedRows, 1);
+});
+
+test('Can modify to insert a new entry', async t => {
+  let result = await db.modify('user', {
+    firstName: 'Billy',
+    lastName: 'Newguy'
+  }, ['firstName', 'NoSuchName']);
+  t.is(result.affectedRows, 1);
+  t.not(result.insertId, 0);
+});
+
+test('Can test existence', async t => {
+  t.truthy(await db.exists('user', ['firstName','Exists']));
+});
+
+test('Can run promisified queries', async t => {
+  let result = await db.query(`select * from user where firstName = 'Exists'`);
+  t.is(result[0].id,8);
+});
+
+test.cb('Can run callback queries', t => {
+  db.query(`select * from user where firstName = 'Exists'`,(err,result) => {
+    t.is(result[0].id,8);
+    t.end();
+  });
+});
+
+test('Can run query providing a connection',async t => {
+  let conn = await db.getConnection();
+  let result = await db.query(`select * from user where firstName = 'Exists'`,null,conn);
+  t.is(result[0].id,8);
+  conn.release();
 });
